b520421444b90104a87d175d04be1327d1394c73
Vladislav Vorobev <vladislav.vorobev@syncplicity.com>
2020-01-27 00:57:21 +0100

    Remove entities not needed

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -5,38 +5,6 @@ import { RouterModule } from '@angular/router';
   imports: [
     RouterModule.forChild([
       {
-        path: 'region',
-        loadChildren: () => import('./region/region.module').then(m => m.BugTrackerJHipsterRegionModule)
-      },
-      {
-        path: 'country',
-        loadChildren: () => import('./country/country.module').then(m => m.BugTrackerJHipsterCountryModule)
-      },
-      {
-        path: 'location',
-        loadChildren: () => import('./location/location.module').then(m => m.BugTrackerJHipsterLocationModule)
-      },
-      {
-        path: 'department',
-        loadChildren: () => import('./department/department.module').then(m => m.BugTrackerJHipsterDepartmentModule)
-      },
-      {
-        path: 'task',
-        loadChildren: () => import('./task/task.module').then(m => m.BugTrackerJHipsterTaskModule)
-      },
-      {
-        path: 'employee',
-        loadChildren: () => import('./employee/employee.module').then(m => m.BugTrackerJHipsterEmployeeModule)
-      },
-      {
-        path: 'job',
-        loadChildren: () => import('./job/job.module').then(m => m.BugTrackerJHipsterJobModule)
-      },
-      {
-        path: 'job-history',
-        loadChildren: () => import('./job-history/job-history.module').then(m => m.BugTrackerJHipsterJobHistoryModule)
-      },
-      {
         path: 'project',
         loadChildren: () => import('./project/project.module').then(m => m.BugTrackerJHipsterProjectModule)
       },
